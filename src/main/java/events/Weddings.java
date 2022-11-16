f1da7177dbfaa3fbaa545da4ae429ff7b2127d6f
Dawid Deszcz <d.deszcz@gmail.com>
2022-11-16 17:47:19 +0100

    Complete Wedding class

--- src/main/java/events/Weddings.java
+++ src/main/java/events/Weddings.java
@@ -1,4 +1,23 @@
 package events;
 
-public class Weddings extends Event{
+import employees.Helper;
+import employees.Organizer;
+import util.Display;
+
+import java.util.List;
+
+public class Weddings extends Event {
+    private boolean weddingCakeReady;
+
+    public Weddings(List<Helper> numberHelpersNeeded, List<Organizer> numberOrganizersNeeded, Organizer chiefOrganizer, boolean weddingCakeReady) {
+        super(numberHelpersNeeded, numberOrganizersNeeded, chiefOrganizer);
+        if (weddingCakeReady) {
+            Display.noCakeNoWedding();
+        } else {
+            this.weddingCakeReady = weddingCakeReady;
+        }
+    }
+
+
+
 }
