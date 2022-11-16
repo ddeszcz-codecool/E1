5873fe90613667cff8709a08e31772392e4ee42a
Dawid Deszcz <d.deszcz@gmail.com>
2022-11-16 18:04:41 +0100

    Add Input and Display methods

--- src/main/java/util/Display.java
+++ src/main/java/util/Display.java
@@ -1,4 +1,32 @@
 package util;
 
 public class Display {
+    public static String notEnoughHelpers(){
+        return "Larger number of helpers is needed to created this event";
+    }
+
+    public static String notEnoughOrganisers(){
+        return "Larger number of organisers is needed to created this event";
+    }
+
+    public static String startMessage() {
+        return "This event has officially started ;-)";
+    }
+
+    public static String howManyDrinksMessage() {
+        return "Please calculate how many drinks will be available in this event";
+    }
+
+
+    public static String noCakeNoWedding() {
+        return "Wedding cannot start without a cake, please organize cake before starting";
+    }
+
+    public static String cannotBeSmallerThanZero() {
+        return "This number cannot be smaller than zero";
+    }
+
+    public static String moreGoodiesBagsNeeded() {
+        return "More goodies bags are needed";
+    }
 }
--- src/main/java/util/Input.java
+++ src/main/java/util/Input.java
@@ -1,4 +1,11 @@
 package util;
 
+import java.util.Scanner;
+
 public class Input {
+
+    public static int numberOfCocktailsAvailable(){
+        Scanner scanner = new Scanner(System.in);
+        return Integer.parseInt(scanner.nextLine());
+    }
 }
