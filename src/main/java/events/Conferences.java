c7a9e517d60df59bb292bb4252c500c9bfc796a9
Dawid Deszcz <d.deszcz@gmail.com>
2022-11-16 18:03:58 +0100

    Complete Conferences class

--- src/main/java/events/Conferences.java
+++ src/main/java/events/Conferences.java
@@ -1,4 +1,43 @@
 package events;
 
+import employees.Helper;
+import employees.Organizer;
+import util.Display;
+
+import java.util.List;
+
+
 public class Conferences extends Event{
+
+    private int goodiesBags;
+    private int numberOfGuests;
+
+
+    public Conferences(List<Helper> numberHelpersNeeded, List<Organizer> numberOrganizersNeeded,
+                       Organizer chiefOrganizer, int goodiesBags, int numberOfGuests) {
+        super(numberHelpersNeeded, numberOrganizersNeeded, chiefOrganizer);
+        if (goodiesBags<0){
+            Display.cannotBeSmallerThanZero();
+        } else {
+        this.goodiesBags = goodiesBags;}
+        if (numberOfGuests<0){
+            Display.cannotBeSmallerThanZero();
+        } else {
+            this.numberOfGuests = numberOfGuests;
+        }
+    }
+
+    @Override
+    public void eventStart() {
+        if(goodiesBags == 0){
+            super.eventStart();
+        } else if (goodiesBags >= numberOfGuests){
+            super.eventStart();
+        } else {
+            Display.moreGoodiesBagsNeeded();
+        }
+
+    }
+
+
 }
