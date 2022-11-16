781e9fce375c2d3ac8b454b03beddd992f3db902
Dawid Deszcz <d.deszcz@gmail.com>
2022-11-16 17:21:08 +0100

    Add Display and Input class

--- src/main/java/events/Event.java
+++ src/main/java/events/Event.java
@@ -1,4 +1,16 @@
 package events;
 
 public class Event {
+    private int numberHelpersNeeded;
+    private int numberOrganizersNeeded;
+    private String chiefOrganizer;
+
+    public Event(int numberHelpersNeeded, int numberOrganizersNeeded, String chiefOrganizer) {
+        if (numberHelpersNeeded<=10){
+
+        }
+        this.numberHelpersNeeded = numberHelpersNeeded;
+        this.numberOrganizersNeeded = numberOrganizersNeeded;
+        this.chiefOrganizer = chiefOrganizer;
+    }
 }
