0f4a5414f217e3d0c95e0bb9d87c26ab9b55b174
Dawid Deszcz <d.deszcz@gmail.com>
2022-11-16 18:04:56 +0100

    Refactor Event class

--- src/main/java/events/Event.java
+++ src/main/java/events/Event.java
@@ -1,16 +1,57 @@
 package events;
 
+import employees.Helper;
+import employees.Organizer;
+import util.Display;
+
+import java.util.List;
+
 public class Event {
-    private int numberHelpersNeeded;
-    private int numberOrganizersNeeded;
-    private String chiefOrganizer;
+    private List<Helper> numberHelperNeeded;
+    private List<Organizer> numberOrganizersNeeded;
+    private Organizer chiefOrganizer;
 
-    public Event(int numberHelpersNeeded, int numberOrganizersNeeded, String chiefOrganizer) {
-        if (numberHelpersNeeded<=10){
-
+    public Event(List<Helper> numberHelpersNeeded, List<Organizer> numberOrganizersNeeded, Organizer chiefOrganizer) {
+        if (numberHelpersNeeded.size()<=10){
+            Display.notEnoughHelpers();
+        }else {
+            this.numberHelperNeeded = numberHelpersNeeded;
         }
-        this.numberHelpersNeeded = numberHelpersNeeded;
+
+        if (numberOrganizersNeeded.size()<=10){
+            Display.notEnoughOrganisers();
+        }else {
+            this.numberOrganizersNeeded = numberOrganizersNeeded;
+        }
+
+        this.chiefOrganizer = chiefOrganizer;
+    }
+
+    public void eventStart(){
+        Display.startMessage();
+    }
+
+    public List<Helper> getNumberHelperNeeded() {
+        return numberHelperNeeded;
+    }
+
+    public void setNumberHelperNeeded(List<Helper> numberHelperNeeded) {
+        this.numberHelperNeeded = numberHelperNeeded;
+    }
+
+    public List<Organizer> getNumberOrganizersNeeded() {
+        return numberOrganizersNeeded;
+    }
+
+    public void setNumberOrganizersNeeded(List<Organizer> numberOrganizersNeeded) {
         this.numberOrganizersNeeded = numberOrganizersNeeded;
+    }
+
+    public Organizer getChiefOrganizer() {
+        return chiefOrganizer;
+    }
+
+    public void setChiefOrganizer(Organizer chiefOrganizer) {
         this.chiefOrganizer = chiefOrganizer;
     }
 }
